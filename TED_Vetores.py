1ff613897a0674736b5aee3416f2056af5e33ea0
Esdras <esdraslucena@hotmail.com>
2023-05-08 15:57:33 -0300

    TED vetores introducao a programacao

---
+++ TED_Vetores.py
@@ -0,0 +1,22 @@
+# cod = 0
+# vetor = []
+# while cod >= 0 and cod <20:
+#     num = int(input('digite um numero: '))
+#     vetor.append(num)
+#     cod=cod+1
+# vetor = vetor[::-1]
+# print(vetor)
+
+
+# VET = []
+# repetido = False
+# for n in range(10):
+#     num = int(input('digite um numero: '))
+#     VET.append(num)
+# for i in range(len(VET)):
+#     for i2 in range(i+1, len(VET)):
+#         if VET[i] == VET[i2]:
+#             print(f'o elemento {VET[i]} está repetido no indíce {i} e {i2} ')
+#             repetido = True
+# if repetido == False:
+#     print('não há nenhum elemento repetido neste vetor.')
